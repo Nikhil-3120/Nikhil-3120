2b23efc764bbb4e590d268f95b4b1d94b743c34b
Nikhil K S <nikhilksmlk3@gmail.com>
2023-08-16 14:03:55 +0530

    Update Pricecompare.java

--- Pricecompare.java
+++ Pricecompare.java
@@ -16,7 +16,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class NewTest
+public class Pricecompare
 {
     WebDriver driver;
     @BeforeMethod
